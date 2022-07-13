c8bcc53e64b7dc3a1373fb47ea4cdfbf2ad50747
Elyas Hedayat <98589320+elyashedayat10@users.noreply.github.com>
2022-07-13 11:29:55 +0430

    Update urls.py

--- wantads/api/urls.py
+++ wantads/api/urls.py
@@ -13,7 +13,7 @@ from .views import (
 
 app_name = "want_ad"
 urlpatterns = [
-    path("", HomeApiView.as_view(), name="home"),
+    path("home/", HomeApiView.as_view(), name="home"),
     path("<int:pk>/", WantAdRetrieveAPIView.as_view(), name="detail_api"),
     path("want_category/<int:id>/", CategoryWantAdApiView.as_view(), name="category"),
     path("recent_seen/", ViewedApiView.as_view(), name="seen"),
